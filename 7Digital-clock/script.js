d7d210252ef51d349faf08f6cc8fb9cc322c8544
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-12-07 20:05:16 +0530

    Refactor Digital Clock script: optimize time retrieval and improve display formatting

--- 7Digital-clock/script.js
+++ 7Digital-clock/script.js
@@ -2,14 +2,21 @@ let hour = document.getElementById("hrs");
 let mins = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-
 setInterval(() => {
     let currentTime = new Date();
 
-    console.log(currentTime);
+    let h = currentTime.getHours();
+    let m = currentTime.getMinutes();
+    let s = currentTime.getSeconds();
 
+    // let ampm = h >= 12 ? "PM" : "AM";
 
-    hour.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
-    mins.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+    // Convert to 12-hour format
+    h = h % 12;
+    h = h ? h : 12; // if hour is 0, set to 12
+
+    hour.innerHTML = (h < 10 ? "0" : "") + h;
+    mins.innerHTML = (m < 10 ? "0" : "") + m;
+    sec.innerHTML = (s < 10 ? "0" : "") + s;
+
 }, 1000);
