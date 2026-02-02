38f3c149724de9e693346140082b865375ddb740
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-02 21:42:55 +0530

    Create: create a project where i can store my all projects in icon format and aslo add a about section for each project
    Add: include new images and SVG files; create index.html and script.js for project structure

---
+++ 11LinkHouse/script.js
@@ -0,0 +1,57 @@
+let cross = document.querySelector(".info>img");
+let about = document.querySelectorAll(".parentBox");
+let icon = document.querySelectorAll(".parentBox");
+
+
+
+
+cross.addEventListener("click", () => {
+    document.querySelector(".info").style.display = "none";
+})
+
+
+
+
+
+let obj = [
+    {
+        "title": "RagRekha",
+        "description": "A home for music that flows through rhythm and emotion. Discover playlists, moods, and sounds that truly resonate"
+    },
+    {
+        "title": "Weather",
+        "description": "Real-time weather updates with clear forecasts and essential details. Stay prepared with accurate conditions, anytime, anywhere"
+    },
+    {
+        "title": "Portfolio",
+        "description": "A personal portfolio showcasing my projects, skills, and work experience. Built to highlight real-world development and problem-solving abilities"
+    },
+]
+
+
+about.forEach(a => {
+    a.addEventListener("click", e => {
+        for (let i = 0; i < obj.length; i++) {
+            if (a.className.includes(obj[i].title)) {
+                document.querySelector(".about").style.display = "block";
+                document.querySelector(".about>h2").innerHTML = "Name - " + obj[i].title;
+                document.querySelector(".about>p").innerHTML = "<b>Description: </b>"+obj[i].description;
+            }
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
