0a1452969b6d3ecbcb485662195a05836b5df841
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-07 10:16:51 +0530

    Add food rendering and update snake growth logic in Snake game

--- 12SnakeGame/script.js
+++ 12SnakeGame/script.js
@@ -8,6 +8,7 @@ const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
 
 let intervalId = null;
+let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) };
 
 
 const blocks = [];
@@ -38,30 +39,25 @@ for (let row = 0; row < rows; row++) {
 
 
 function renderSnake() {
-    snake.forEach(segment => {
-        blocks[`${segment.x},${segment.y}`].classList.add("fill");
-    });
-}
-
-
-intervalId = setInterval(() => {
     let head = null;
+    blocks[`${food.x},${food.y}`].classList.add("food");
+
     if (direction == "left") {
         head = {
             x: snake[0].x,
             y: snake[0].y - 1
         }
-    }else if (direction == "right") {
+    } else if (direction == "right") {
         head = {
             x: snake[0].x,
             y: snake[0].y + 1
         }
-    }else if (direction == "down") {
+    } else if (direction == "down") {
         head = {
             x: snake[0].x + 1,
             y: snake[0].y
         }
-    }else if (direction == "up") {
+    } else if (direction == "up") {
         head = {
             x: snake[0].x - 1,
             y: snake[0].y
@@ -75,6 +71,14 @@ intervalId = setInterval(() => {
     }
 
 
+    if (head.x == food.x && head.y == food.y) {
+        blocks[`${food.x},${food.y}`].classList.remove("food");
+        food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) };
+        blocks[`${food.x},${food.y}`].classList.add("food");
+        snake.unshift(head);
+    }
+
+
     snake.forEach(segment => {
         blocks[`${segment.x},${segment.y}`].classList.remove("fill");
     });
@@ -82,6 +86,15 @@ intervalId = setInterval(() => {
     snake.unshift(head);
     snake.pop();
 
+
+
+    snake.forEach(segment => {
+        blocks[`${segment.x},${segment.y}`].classList.add("fill");
+    });
+}
+
+
+intervalId = setInterval(() => {
     renderSnake();
 }, 400);
 
@@ -89,11 +102,11 @@ intervalId = setInterval(() => {
 addEventListener("keydown", (event) => {
     if (event.key == "ArrowUp") {
         direction = "up";
-    }else if (event.key == "ArrowDown") {
+    } else if (event.key == "ArrowDown") {
         direction = "down";
-    }else if (event.key == "ArrowLeft") {
+    } else if (event.key == "ArrowLeft") {
         direction = "left";
-    }else if (event.key == "ArrowRight") {
+    } else if (event.key == "ArrowRight") {
         direction = "right";
     }
 });
