dafe3d382471eee8ab3e583414bb5141dcd4aa95
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-08 14:19:50 +0530

    Add drag-and-drop functionality to Kanban board; enhance task column styles and modal for adding tasks

---
+++ 13KanbanBoard/script.js
@@ -0,0 +1,49 @@
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+let dragElement = null;
+
+// console.log(todo, progress, done);
+
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach(task => {
+    task.addEventListener("dragenter", (e) => {
+        // console.log("dragenter", e);
+        dragElement = task;
+    });
+})
+
+
+function addDragEventOnColumn(column) {
+    column.addEventListener("dragenter", (e) => {
+        // console.log("dragenter", e);
+        e.preventDefault();
+        column.classList.add("hover-over");
+    });
+
+    column.addEventListener("dragleave", (e) => {
+        // console.log("dragleave", e);
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    });
+    
+    
+    column.addEventListener("dragover", (e) => {
+        // console.log("dragover", e);
+        e.preventDefault();
+    });
+
+    column.addEventListener("drop", (e) => {
+        // console.log("drop", e);
+        e.preventDefault();
+        // console.log("Dropped",dragElement,column);
+        column.appendChild(dragElement);
+        column.classList.remove("hover-over");
+    });
+}
+
+addDragEventOnColumn(todo);
+addDragEventOnColumn(progress);
+addDragEventOnColumn(done);
