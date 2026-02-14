960759f135c67442b583e46b80e14b68f1d1c807
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-14 14:14:28 +0530

    Refactor Caps Lock functionality and improve state management in Keyboard Simulator

--- 15Keyboard/script.js
+++ 15Keyboard/script.js
@@ -1,16 +1,25 @@
 let allKeys = document.querySelectorAll('.key');
+let capsLight = document.querySelector('.capslockLight');
 
 
 if (window.innerWidth < 600) {
     alert("This keyboard simulator is best viewed on a desktop or laptop. Some keys may not work properly on mobile devices.");
 }
 
-
+if (localStorage.getItem("capsState") === "on") {
+    capsLight.classList.add("capslockLightOn");
+}
 
 document.addEventListener('keydown', function (event) {
     allKeys.forEach(function (key) {
         if (event.key === "CapsLock" && key.id.toLowerCase() == "keycapslock") {
-            document.querySelector('.capslockLight').classList.toggle('capslockLightOn');
+
+            capsLight.classList.toggle('capslockLightOn');
+            localStorage.setItem(
+                "capsState",
+                capsLight.classList.contains("capslockLightOn") ? "on" : "off"
+            );
+
             event.preventDefault(); // Prevent the default behavior of CapsLock key
 
         }
