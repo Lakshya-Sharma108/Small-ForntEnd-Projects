0c8c04e24d7590083ffd2b04e01d40c2437a17f3
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-14 13:39:51 +0530

    Update textarea styling and improve Caps Lock functionality in Keyboard Simulator

--- 15Keyboard/script.js
+++ 15Keyboard/script.js
@@ -1,8 +1,20 @@
 let allKeys = document.querySelectorAll('.key');
 
 
+if (window.innerWidth < 600) {
+    alert("This keyboard simulator is best viewed on a desktop or laptop. Some keys may not work properly on mobile devices.");
+}
+
+
+
 document.addEventListener('keydown', function (event) {
     allKeys.forEach(function (key) { 
+        if (event.key === "CapsLock" && key.id.toLowerCase() == "keycapslock") {
+            document.querySelector('.capslockLight').classList.toggle('capslockLightOn');
+            event.preventDefault(); // Prevent the default behavior of CapsLock key
+            
+        }
+        
         if (key.id.toLowerCase() == "key"+event.key.toLowerCase() || (event.key == " " && key.id == "keySpace")) {
             key.classList.add('active');
         }
