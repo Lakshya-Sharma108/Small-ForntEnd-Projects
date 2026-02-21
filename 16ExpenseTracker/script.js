70c1a072ba72567f2f10369a2a4ac9a7cc166eee
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-21 19:40:28 +0530

    Initialize variables for balance, income, expense, and transaction inputs in Expense Tracker

---
+++ 16ExpenseTracker/script.js
@@ -0,0 +1,14 @@
+let balance = document.querySelector(".totalBalance h3");
+let income = document.querySelector(".totalIncome h3");
+let expense = document.querySelector(".totalExpense h3");
+
+
+let transactionTitle = document.querySelector(".transactionTitle input");
+let transactionAmount = document.querySelector(".transactionAmount input");
+let category = document.querySelector(".category select").value;
+let addTransactionBtn = document.querySelector(".addTransactionBtn");
+let date = document.querySelector(".date input");
+
+let totalIncome = 0;
+let totalExpense = 0;
+let totalBalance = 0;
