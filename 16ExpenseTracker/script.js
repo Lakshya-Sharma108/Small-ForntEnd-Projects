74a3e7cb5c2b5e188546dba8c318edf073b257b7
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-21 19:58:49 +0530

    Implement transaction addition functionality and update styles for transaction history in Expense Tracker

--- 16ExpenseTracker/script.js
+++ 16ExpenseTracker/script.js
@@ -6,9 +6,30 @@ let expense = document.querySelector(".totalExpense h3");
 let transactionTitle = document.querySelector(".transactionTitle input");
 let transactionAmount = document.querySelector(".transactionAmount input");
 let category = document.querySelector(".category select").value;
-let addTransactionBtn = document.querySelector(".addTransactionBtn");
+let addTransactionBtn = document.querySelector(".addButton button");
 let date = document.querySelector(".date input");
 
 let totalIncome = 0;
 let totalExpense = 0;
 let totalBalance = 0;
+
+
+addTransactionBtn.addEventListener("click", function () {
+    let historyCard = document.createElement("div");
+    historyCard.innerHTML = `<div class="transaction">
+                <div class="transactionDetails">
+                    <h4>${transactionTitle.value}</h4>
+                    <p>₹${transactionAmount.value}</p>
+                    <button>Delete</button>
+                </div>
+                <div class="transactionDate">
+                    <p>${date.value}</p>
+                </div>
+            </div>`;
+
+
+    
+    
+
+    document.querySelector(".TransactionHistory").append(historyCard);
+});
