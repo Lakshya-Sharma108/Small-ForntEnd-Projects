c6ce7f24bd9cabd332dbcd400aeb61ff03f36f85
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-02-22 20:03:49 +0530

    Update initial balance and enhance transaction input styles in Expense Tracker

--- 16ExpenseTracker/script.js
+++ 16ExpenseTracker/script.js
@@ -5,16 +5,24 @@ let expense = document.querySelector(".totalExpense h3");
 
 let transactionTitle = document.querySelector(".transactionTitle input");
 let transactionAmount = document.querySelector(".transactionAmount input");
-let category = document.querySelector(".category select").value;
 let addTransactionBtn = document.querySelector(".addButton button");
 let date = document.querySelector(".date input");
 
+// let cardAmount = document.querySelector(".transactionDetails p");
+
 let totalIncome = 0;
 let totalExpense = 0;
-let totalBalance = 0;
+let totalBalance = 3000;
 
+
+expense.textContent = `₹${totalExpense}`;
+income.textContent = `₹${totalIncome}`;
+balance.textContent = `₹${totalBalance}`;
 
 addTransactionBtn.addEventListener("click", function () {
+    let type = document.querySelector("#type").value;
+    let category = document.querySelector(".category select").value;
+
     let historyCard = document.createElement("div");
     historyCard.innerHTML = `<div class="transaction">
                 <div class="transactionDetails">
@@ -28,4 +36,30 @@ addTransactionBtn.addEventListener("click", function () {
             </div>`;
 
     document.querySelector(".TransactionHistory").append(historyCard);
+
+
+    if (type == "expense") {
+        console.log("hello ji");
+        if (balance.textContent == "₹0" || parseInt(balance.textContent.slice(1)) <= parseInt(transactionAmount.value)) {
+            alert("You don't have enough balance to add an expense!");
+        } else {
+            totalExpense += parseInt(transactionAmount.value);
+            expense.textContent = `₹${totalExpense}`;
+            totalBalance -= parseInt(transactionAmount.value);
+            balance.textContent = `₹${totalBalance}`;
+            historyCard.querySelector(".transactionDetails p").style.color = "red";
+        }
+    } else if (type == "income") {
+        totalIncome += parseInt(transactionAmount.value);
+        income.textContent = `₹${totalIncome}`;
+        totalBalance += parseInt(transactionAmount.value);
+        balance.textContent = `₹${totalBalance}`;
+        historyCard.querySelector(".transactionDetails p").style.color = "green";
+    }
+
+    // Clear input fields after adding transaction
+    transactionTitle.value = "";
+    transactionAmount.value = "";
+    date.value = "";
 });
+
